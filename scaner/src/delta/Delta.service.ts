cb96837ad240f8e00c705d799ad5919bad752e46
Paul Saf <hollysaf@yahoo.com>
2023-02-10 13:42:05 +0300

    fixed negative offset error

--- scaner/src/delta/Delta.service.ts
+++ scaner/src/delta/Delta.service.ts
@@ -15,18 +15,35 @@ export class DeltaService {
     let rows = await this.DeltaRepository
     .countBy({coin: name })
     if(tmfr === '1-H') {
-      return await this.DeltaRepository
-      .createQueryBuilder('delta')
-      .where('delta.coin = :name', { name })
-      .orderBy('id').skip(rows-60)
-      .getMany();
+      if(rows - 60) {
+        return await this.DeltaRepository
+        .createQueryBuilder('delta')
+        .where('delta.coin = :name', { name })
+        .orderBy('id').skip(rows-60)
+        .getMany();
+      } else {
+        return await this.DeltaRepository
+        .createQueryBuilder('delta')
+        .where('delta.coin = :name', { name })
+        .orderBy('id')
+        .getMany();
+      }
+
     } else {
       let multiply = Number(tmfr.split('-')[0])
-      return await this.DeltaRepository
-      .createQueryBuilder('delta')
-      .where('delta.coin = :name', { name })
-      .orderBy('id').skip(rows - 60 * multiply)
-      .getMany();
+      if(rows - 60 * multiply) {
+        return await this.DeltaRepository
+        .createQueryBuilder('delta')
+        .where('delta.coin = :name', { name })
+        .orderBy('id').skip(rows - 60 * multiply)
+        .getMany();
+      } else {
+        return await this.DeltaRepository
+        .createQueryBuilder('delta')
+        .where('delta.coin = :name', { name })
+        .orderBy('id')
+        .getMany();
+      }
     }
 
   }
