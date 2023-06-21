32c76dc163e2ec53c0f7fe3e4e10de0f3140cee8
unknown <hollysaf@yahoo.com>
2023-06-21 12:54:37 +0300

    refactor deltaCount func

--- index.ts
+++ index.ts
@@ -1,16 +1,14 @@
-
 import fetch from "node-fetch";
 import "reflect-metadata";
 import * as dotenv from "dotenv";
 dotenv.config();
-import fs from 'fs'
+import fs from "fs";
 
 import { DataSource } from "typeorm";
 import { AggTrade } from "./models/AggTrades.js";
 import { Delta } from "./models/Delta.js";
-import { TrackingCoins } from "./models/TrackingCoins.js"
+import { TrackingCoins } from "./models/TrackingCoins.js";
 import { createBot } from "./bot.js";
-
 
 const AppDataSource = new DataSource({
   type: "postgres",
@@ -37,7 +35,7 @@ interface addTradesresp {
 }
 
 async function load(coin: string) {
-  let url =  `https://data.binance.com/api/v3/aggTrades?symbol=${coin}USDT&limit=400`;
+  let url = `https://data.binance.com/api/v3/aggTrades?symbol=${coin}USDT&limit=400`;
   let result = await fetch(url);
   if (result.ok) {
     let response = await result.json();
@@ -59,132 +57,131 @@ const checkApi = async (coin: string) => {
 
 const trackCoin = async (datasourse: DataSource, coin: string) => {
   await datasourse.manager
-  .createQueryBuilder()
-  .insert()
-  .into(TrackingCoins)
-  .values({coin: coin})
-  .execute()
-}
+    .createQueryBuilder()
+    .insert()
+    .into(TrackingCoins)
+    .values({ coin: coin })
+    .execute();
+};
 
 const readTrackingCoins = async (datasourse: DataSource) => {
   return await datasourse.manager
-  .createQueryBuilder()
-  .select("tracking_coins.coin")
-  .from(TrackingCoins, "tracking_coins")
-  .getMany()
-}
+    .createQueryBuilder()
+    .select("tracking_coins.coin")
+    .from(TrackingCoins, "tracking_coins")
+    .getMany();
+};
 
-const checkTrackingCoins = async (datasourse : DataSource, coin :string) => {
+const checkTrackingCoins = async (datasourse: DataSource, coin: string) => {
   let result = await datasourse.manager
-  .createQueryBuilder()
-  .select("tracking_coins.coin")
-  .from(TrackingCoins, "tracking_coins")
-  .where("tracking_coins.coin = :coin", {coin: coin})
-  .getOne()
-  if(result) {
-    return true
+    .createQueryBuilder()
+    .select("tracking_coins.coin")
+    .from(TrackingCoins, "tracking_coins")
+    .where("tracking_coins.coin = :coin", { coin: coin })
+    .getOne();
+  if (result) {
+    return true;
   } else {
-    return false
+    return false;
   }
-}
-
-
-
+};
 
 AppDataSource.initialize()
   .then(async () => {
-
-    let maker = 0;
-    let taker = 0;
+    let purchases = 0;
+    let sells = 0;
     let timecounter = 100;
-    let date = '';
+    let date = ''
+    
 
-    const countDelta = (isMaker:boolean, quantity:number, price:number) => {
-      isMaker ? (maker += quantity * price) : (taker += quantity * price);
+    const countDelta = (
+      isMaker: boolean,
+      quantity: number,
+      price: number
+    ) => {
+      if (isMaker) {
+        sells += quantity * price;
+      } else {
+        purchases += quantity * price;
+      }
     };
 
-    const getDelta = async (el:addTradesresp, coin: string) => {
-
-      let ts = Number(el.T)
+    const getDelta = async (el: addTradesresp, coin: string) => {
+      let ts = Number(el.T);
       let hours = new Date(ts).getHours();
-      
-      
-        if (timecounter === 100) {
-          timecounter = hours;
-          date = new Date(ts).toLocaleDateString()
-          countDelta(el.m, Number(el.q), Number(el.p));
 
+      if (timecounter === 100) {
+        timecounter = hours;
+        date = new Date(ts).toLocaleDateString();
+        countDelta(el.m, Number(el.q), Number(el.p));
+      } else {
+        if (timecounter === hours) {
+          countDelta(el.m, Number(el.q), Number(el.p));
         } else {
-          if (timecounter === hours) {
-            countDelta(el.m, Number(el.q), Number(el.p));
-          } else {
-            await AppDataSource.manager
+          await AppDataSource.manager
             .createQueryBuilder()
             .insert()
             .into(Delta)
             .values({
               coin: coin,
-              delta: marketBuy - limitBuy,
-              ts: el.T
+              delta: sells - purchases,
+              ts: el.T,
             })
             .execute();
 
-            marketBuy = 0;
-            limitBuy = 0;
-            timecounter = hours;
-            date = new Date(ts).toLocaleDateString()
-            countDelta(el.m, Number(el.q), Number(el.p));
-          }
+          purchases = 0;
+          sells = 0;
+          timecounter = hours;
+          date = new Date(ts).toLocaleDateString();
+          countDelta(el.m, Number(el.q), Number(el.p));
         }
-    }
+      }
+    };
 
     const loader = (coin: string) => {
       setInterval(() => {
         load(coin).then(
           async (data: any) => {
             for (const el of data) {
-            //   const trade = await AppDataSource.manager
-            //   .createQueryBuilder(AggTrade, "agg_trade")
-            //   .where("agg_trade.id = :id", { id: el.a })
-            //   .getOne();
-            // if (trade) {
-            //   return;
+              //   const trade = await AppDataSource.manager
+              //   .createQueryBuilder(AggTrade, "agg_trade")
+              //   .where("agg_trade.id = :id", { id: el.a })
+              //   .getOne();
+              // if (trade) {
+              //   return;
 
-            // } else {
+              // } else {
 
-              
-            //   await AppDataSource.manager
-            //     .createQueryBuilder()
-            //     .insert()
-            //     .into(AggTrade)
-            //     .values({
-            //       id: el.a,
-            //       name: `${coin}-USDT`,
-            //       price: el.p,
-            //       quantity: el.q,
-            //       timeMachine: el.T,
-            //       time: new Date(el.T).toLocaleString(),
-            //       isBuyer: el.m,
-            //       isBest: el.M,
-            //     })
-            //     .execute();
-            // }
-            await getDelta(el, coin);
+              //   await AppDataSource.manager
+              //     .createQueryBuilder()
+              //     .insert()
+              //     .into(AggTrade)
+              //     .values({
+              //       id: el.a,
+              //       name: `${coin}-USDT`,
+              //       price: el.p,
+              //       quantity: el.q,
+              //       timeMachine: el.T,
+              //       time: new Date(el.T).toLocaleString(),
+              //       isBuyer: el.m,
+              //       isBest: el.M,
+              //     })
+              //     .execute();
+              // }
+              await getDelta(el, coin);
             }
           },
           (err) => {
-            fs.writeFileSync("log.txt", new Date().toLocaleDateString() + ' ' + err.message);
+            fs.writeFileSync(
+              "log.txt",
+              new Date().toLocaleDateString() + " " + err.message
+            );
             console.log(err);
           }
         );
       }, 60000);
-      
-      
-      
     };
 
-
-    
     const token = process.env.BOT_TOKEN ? process.env.BOT_TOKEN : "";
 
     const bot = createBot(token);
@@ -197,26 +194,29 @@ AppDataSource.initialize()
       });
 
       bot.on("text", async (ctx) => {
-        let coin = ctx.message.text.toLocaleUpperCase()
-        if (await checkApi(coin) && ! await checkTrackingCoins(AppDataSource, coin)) {
+        let coin = ctx.message.text.toLocaleUpperCase();
+        if (
+          (await checkApi(coin)) &&
+          !(await checkTrackingCoins(AppDataSource, coin))
+        ) {
           loader(coin);
           trackCoin(AppDataSource, coin);
           await ctx.reply(`Now ${coin} is tracking`);
-        } else if(! await checkApi(coin)) {
+        } else if (!(await checkApi(coin))) {
           await ctx.reply(`Enter right coin please`);
         } else {
           await ctx.reply(`Coin is tracked already`);
         }
       });
-      
+
       bot.launch();
     }
 
-    const coins = await readTrackingCoins(AppDataSource)
-    coins.forEach(el=>{
-      loader(el.coin)
-    })
- 
+    const coins = await readTrackingCoins(AppDataSource);
+    coins.forEach((el) => {
+      loader(el.coin);
+    });
+
     // here you can start to work with your database
   })
   .catch((error) => console.log(error));
